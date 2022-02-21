11477f883811164ddd3e9312f612e75adc59910a
user3 <qwerty123>
2022-02-21 22:15:01 +0700

    added generation of n*n reversible function

--- VectorBoolFunction.go
+++ VectorBoolFunction.go
@@ -2,16 +2,24 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"math/rand"
+	"strconv"
 	"time"
+	"unsafe"
 )
 
+//not more than 32
+type block uint8
+
 type BoolFunction struct {
-	value      [][]uint16
+	value [][]block
+	n     int
+	m     int
+	rows  int
+
 	mBlockSize int
-	n          int
-	m          int
-	len        int
+	blockSize  int
 }
 
 //вычисляет n из длины
@@ -55,22 +63,22 @@ func log2(len int) (i int) {
 //
 ////конструктор по строке
 //func newBF(str string) (BoolFunction, error) {
-//	len := len(str)
-//	if len > 1 && (len&(len-1)) == 0 {
+//	rows := rows(str)
+//	if rows > 1 && (rows&(rows-1)) == 0 {
 //		var bf BoolFunction
-//		if len < 16 {
+//		if rows < 16 {
 //			bf = BoolFunction{
 //				make([]uint16, 1),
 //				1,
-//				log2(len),
-//				len,
+//				log2(rows),
+//				rows,
 //			}
 //		} else {
 //			bf = BoolFunction{
-//				make([]uint16, len/16),
-//				len / 16,
-//				log2(len),
-//				len,
+//				make([]uint16, rows/16),
+//				rows / 16,
+//				log2(rows),
+//				rows,
 //			}
 //		}
 //		for i, v := range str {
@@ -81,8 +89,8 @@ func log2(len int) (i int) {
 //			bf.value[block] <<= 1
 //			bf.value[block] |= uint16(v - '0')
 //		}
-//		if len < 16 {
-//			bf.value[0] <<= 16 - len
+//		if rows < 16 {
+//			bf.value[0] <<= 16 - rows
 //		}
 //		return bf, nil
 //	} else {
@@ -90,41 +98,99 @@ func log2(len int) (i int) {
 //	}
 //}
 
+func (b block) String() string {
+	blockSize := int(unsafe.Sizeof(block(0)) * 8)
+	formatString := "%0" + strconv.Itoa(blockSize) + "b"
+	return fmt.Sprintf(formatString, b)
+}
+
+func (b *block) swap(j int, k int) {
+	blockSize := int(unsafe.Sizeof(block(0)) * 8)
+	/* Move all bits of first set to rightmost side */
+	set1 := (*b >> (blockSize - j - 1)) & 1
+
+	/* Move all bits of second set to rightmost side */
+	set2 := (*b >> (blockSize - k - 1)) & 1
+
+	/* Xor the two sets */
+	Xor := set1 ^ set2
+
+	/* Put the Xor bits back to their original positions */
+	Xor = (Xor << j) | (Xor << k)
+
+	/* Xor the 'Xor' with the original number so that the
+	   two sets are swapped */
+	result := *b ^ Xor
+	*b = result
+}
+
 //конструктор рандомный по длине
-func newRandomBF(n, m int) (BoolFunction, error) {
+func newRandomVBF(n, m int) (BoolFunction, error) {
 	rand.Seed(time.Now().UnixNano())
-	if n > 15 {
+
+	blockSize := int(unsafe.Sizeof(block(0)) * 8)
+	if n > blockSize-1 {
 		return BoolFunction{}, errors.New("too much variables")
 	}
 	len := 1 << n
 
 	bf := BoolFunction{
-		value:      make([][]uint16, len),
-		mBlockSize: (m + 16 - 1) / 16,
-		len:        len,
+		value:      make([][]block, len),
+		mBlockSize: (m + blockSize - 1) / blockSize,
+		rows:       len,
 		n:          n,
 		m:          m,
+		blockSize:  blockSize,
 	}
 
-	diff := (bf.mBlockSize * 16) - m
-
+	diff := (bf.mBlockSize * blockSize) - m
+	fmt.Println("diff = ", diff)
 	for i := 0; i < len; i++ {
-		bf.value[i] = make([]uint16, bf.mBlockSize)
+		bf.value[i] = make([]block, bf.mBlockSize)
 		for j, _ := range bf.value[i] {
-			bf.value[i][j] = uint16(rand.Intn(1 << 16))
+			bf.value[i][j] = block(rand.Intn(1 << blockSize))
 			if diff > 0 && j == bf.mBlockSize-1 {
 				bf.value[i][j] <<= diff
 			}
+
+			fmt.Print(bf.value[i][j], "|")
 		}
+		fmt.Println()
 	}
 	return bf, nil
 }
 
-func newRevBoolFunction(n, m int) (BoolFunction, error) {
+func newRevVBF(n, m int) (BoolFunction, error) {
+	rand.Seed(time.Now().UnixNano())
+	blockSize := int(unsafe.Sizeof(block(0)) * 8)
+	if n > blockSize-1 {
+		return BoolFunction{}, errors.New("too much variables")
+	}
+
+	len := 1 << n
+	bf := BoolFunction{
+		value:      make([][]block, len),
+		mBlockSize: (m + blockSize - 1) / blockSize,
+		rows:       len,
+		n:          n,
+		m:          m,
+		blockSize:  blockSize,
+	}
 	if n < m {
-
+		//todo
 	} else if n == n {
-
+		if bf.mBlockSize > 1 {
+			return BoolFunction{}, errors.New("too much variables")
+		}
+		for i := 0; i < n; i++ {
+			bf.value[i] = make([]block, bf.mBlockSize)
+			bf.value[i][0] = block(i)
+			for j := bf.blockSize; j > 0; j-- {
+				k := rand.Intn(j + 1)
+				bf.value[i][0].swap(j, k)
+			}
+		}
+		return bf, nil
 	}
 	return BoolFunction{}, errors.New("n > m")
 }
--- main.go
+++ main.go
@@ -3,6 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	bf, _ := newRandomBF(3, 20)
+	bf, _ := newRevVBF(5, 20)
 	fmt.Println(bf)
 }
