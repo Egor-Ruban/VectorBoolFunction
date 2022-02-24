da675db7298be5443b9ccdad665cce3dd871174f
user3 <qwerty123>
2022-02-24 19:38:35 +0700

    Расставил комментарии

--- VectorBoolFunction.go
+++ VectorBoolFunction.go
@@ -9,23 +9,29 @@ import (
 	"unsafe"
 )
 
-//not more than 32
+//block - целочисленное значение, в котормо хранится значение функции.
+//может быть длиной 8, 16 или 32 бита (64 тоже можно, но генерация обратимой n*m функции сломается)
+//значения хранятся "слева направо". Значение нулевой (первой) функции хранится в старшем разряде
 type block uint32
 
+//blockSize() - функция, возвращающая длину блока в битах
 func blockSize() int {
 	return int(unsafe.Sizeof(block(0)) * 8)
 }
 
-type BoolFunction struct {
-	value     []block
-	n         int
-	m         int
-	rows      int
-	wasteBits int
+//VectorBoolFunction - структура, хранящая векторную булеву функцию
+type VectorBoolFunction struct {
+	//value - массив значений функции
+	value []block
+	n     int
+	m     int
+	rows  int
+
+	wastedBits int //wastedBits - сколько в блоке незначащих бит в конце
 }
 
-//подсчитывает вес вектора
-func (bf BoolFunction) getWeight() []int {
+//getWeight() - подсчитывает вес каждой кординаты. Возвращает массив с полученными значениями
+func (bf VectorBoolFunction) getWeight() []int {
 	weight := make([]int, bf.m)
 	for _, v := range bf.value {
 		for i := 0; i < bf.m; i++ {
@@ -35,73 +41,83 @@ func (bf BoolFunction) getWeight() []int {
 	return weight
 }
 
-//красивый вывод в виде таблички
-func (bf BoolFunction) String() string {
+//Реализация Интерфейса stringer. Аналог перегрузки оператора вывода
+//Представляет функцию в виде таблицы с указанием значений переменных
+func (bf VectorBoolFunction) String() string {
 	res := ""
-	diff := blockSize() - bf.m
-	formatString2 := "%0" + strconv.Itoa(bf.n) + "b : "            //format string for variables
-	formatString3 := "%0" + strconv.Itoa(blockSize()-diff) + "b\n" //format string if last block is not filled
+	formatString2 := "%0" + strconv.Itoa(bf.n) + "b : "                     //Форматная строка для таблицы переменных
+	formatString3 := "%0" + strconv.Itoa(blockSize()-bf.wastedBits) + "b\n" //Форматная строка для значений функции
 
 	for i, v := range bf.value {
-		res += fmt.Sprintf(formatString2, i) //можно убрать, если не нужен вывод значений переменных
-		res += fmt.Sprintf(formatString3, v>>diff)
+		res += fmt.Sprintf(formatString2, i) // строку можно убрать, если не нужен вывод значений переменных
+		res += fmt.Sprintf(formatString3, v>>bf.wastedBits)
 	}
 	return res
 }
 
-//конструктор рандомный по длине
-func newRandomVBF(n, m int) (BoolFunction, error) {
+//newRandomVBF() - генерирует случайную векторную булеву функцию по заданным n и m
+func newRandomVBF(n, m int) (VectorBoolFunction, error) {
 	rand.Seed(time.Now().UnixNano())
 
+	//если n или m больше размера блока, то возвращает ошибку
 	if n > blockSize() || m > blockSize() {
-		return BoolFunction{}, errors.New("n or m is too big")
+		return VectorBoolFunction{}, errors.New("n or m is too big")
 	}
 
-	bf := BoolFunction{
-		value:     make([]block, 1<<n),
-		rows:      1 << n,
-		n:         n,
-		m:         m,
-		wasteBits: blockSize() - m,
+	//создание объекта функции
+	bf := VectorBoolFunction{
+		value:      make([]block, 1<<n), //создаём массив на 2^n элементов
+		rows:       1 << n,
+		n:          n,
+		m:          m,
+		wastedBits: blockSize() - m,
 	}
 
 	for i := 0; i < bf.rows; i++ {
-		bf.value[i] = block(rand.Intn(1<<m)) << bf.wasteBits
+		bf.value[i] = block(rand.Intn(1<<m)) << bf.wastedBits //генерирует m бит и сдвигает их к старшим разрядам
 	}
 	return bf, nil
 }
 
-func newRevVBF(n, m int) (BoolFunction, error) {
+//newRevVBF() - генерирует случайную обратимую векторную булеву функкцию по заданным n и m
+func newRevVBF(n, m int) (VectorBoolFunction, error) {
 	rand.Seed(time.Now().UnixNano())
+
+	//если n или m больше размера блока, то возвращает ошибку
 	if n > blockSize() || m > blockSize() {
-		return BoolFunction{}, errors.New("n or m is too big")
+		return VectorBoolFunction{}, errors.New("n or m is too big")
 	}
 
-	bf := BoolFunction{
-		value:     make([]block, 1<<n),
-		rows:      1 << n,
-		n:         n,
-		m:         m,
-		wasteBits: blockSize() - m,
+	//создание объекта функции
+	bf := VectorBoolFunction{
+		value:      make([]block, 1<<n), //создаём массив на 2^n элементов
+		rows:       1 << n,
+		n:          n,
+		m:          m,
+		wastedBits: blockSize() - m,
 	}
 
 	if n < m {
+		//Если n<m, то генерируем m случайных неповторяющихся значений и сохраняем
 		l := ((uint64(1) << m) + 64 - 1) / 64 //[2^m/64] - сколько блоков по 64 надо для 2^m бит
-		isTaken := make([]uint64, l)
+		isTaken := make([]uint64, l)          //вектор с флагами, указывающими на то, какие значения уже были использованы
 
 		for i := 0; i < bf.rows; i++ {
 			x := rand.Intn(1 << m)
+			//проверка был ли x использован раньше
 			if (isTaken[x/64]>>(64-x%64-1))&1 == 1 {
 				i--
 			} else {
-				bf.value[i] = block(x << bf.wasteBits)
+				bf.value[i] = block(x << bf.wastedBits)
 				isTaken[x/64] |= uint64(1) << (64 - x%64 - 1)
 			}
 		}
 		return bf, nil
-	} else if n == n {
+	} else if m == n {
+		//Если m == n, то сначала таблица заполняется значениями из множества {0,1)^n по порядку
+		//а затем выполняется случайная перестановка
 		for i := 0; i < bf.rows; i++ {
-			bf.value[i] = block(i) << bf.wasteBits
+			bf.value[i] = block(i) << bf.wastedBits
 		}
 		for i := bf.rows - 1; i > 0; i-- {
 			j := rand.Intn(i + 1)
@@ -111,16 +127,17 @@ func newRevVBF(n, m int) (BoolFunction, error) {
 		}
 		return bf, nil
 	}
-	return BoolFunction{}, errors.New("n > m")
+	return VectorBoolFunction{}, errors.New("n > m")
 }
 
-func (bf BoolFunction) shiftDown(k int) BoolFunction {
-	t := BoolFunction{
-		value:     make([]block, bf.rows),
-		rows:      bf.rows,
-		n:         bf.n,
-		m:         bf.m,
-		wasteBits: blockSize() - bf.m,
+//shiftDown() - метод, сдвигающий значения функции "Вниз" по таблице
+func (bf VectorBoolFunction) shiftDown(k int) VectorBoolFunction {
+	t := VectorBoolFunction{
+		value:      make([]block, bf.rows),
+		rows:       bf.rows,
+		n:          bf.n,
+		m:          bf.m,
+		wastedBits: blockSize() - bf.m,
 	}
 
 	for i := bf.rows - 1; i >= k; i-- {
@@ -129,13 +146,14 @@ func (bf BoolFunction) shiftDown(k int) BoolFunction {
 	return t
 }
 
-func (bf BoolFunction) shiftUp(k int) BoolFunction {
-	t := BoolFunction{
-		value:     make([]block, bf.rows),
-		rows:      bf.rows,
-		n:         bf.n,
-		m:         bf.m,
-		wasteBits: blockSize() - bf.m,
+//shiftUp() - метод, сдвигающий значения функции "Вверх" по таблице
+func (bf VectorBoolFunction) shiftUp(k int) VectorBoolFunction {
+	t := VectorBoolFunction{
+		value:      make([]block, bf.rows),
+		rows:       bf.rows,
+		n:          bf.n,
+		m:          bf.m,
+		wastedBits: blockSize() - bf.m,
 	}
 
 	for i := 0; i < bf.rows-k; i++ {
@@ -144,13 +162,14 @@ func (bf BoolFunction) shiftUp(k int) BoolFunction {
 	return t
 }
 
-func (bf BoolFunction) xor(bf2 BoolFunction) BoolFunction {
-	t := BoolFunction{
-		value:     make([]block, bf.rows),
-		rows:      bf.rows,
-		n:         bf.n,
-		m:         bf.m,
-		wasteBits: blockSize() - bf.m,
+//xor() - покомпонентное сложение по модулю два двух таблиц значений ВБФ
+func (bf VectorBoolFunction) xor(bf2 VectorBoolFunction) VectorBoolFunction {
+	t := VectorBoolFunction{
+		value:      make([]block, bf.rows),
+		rows:       bf.rows,
+		n:          bf.n,
+		m:          bf.m,
+		wastedBits: blockSize() - bf.m,
 	}
 
 	for i := 0; i < bf.rows; i++ {
@@ -159,13 +178,14 @@ func (bf BoolFunction) xor(bf2 BoolFunction) BoolFunction {
 	return t
 }
 
-func (bf BoolFunction) and(bf2 BoolFunction) BoolFunction {
-	t := BoolFunction{
-		value:     make([]block, bf.rows),
-		rows:      bf.rows,
-		n:         bf.n,
-		m:         bf.m,
-		wasteBits: blockSize() - bf.m,
+//and() - покомпонентное умножение двух таблиц значений ВБФ
+func (bf VectorBoolFunction) and(bf2 VectorBoolFunction) VectorBoolFunction {
+	t := VectorBoolFunction{
+		value:      make([]block, bf.rows),
+		rows:       bf.rows,
+		n:          bf.n,
+		m:          bf.m,
+		wastedBits: blockSize() - bf.m,
 	}
 
 	for i := 0; i < bf.rows; i++ {
@@ -174,13 +194,14 @@ func (bf BoolFunction) and(bf2 BoolFunction) BoolFunction {
 	return t
 }
 
-func (bf BoolFunction) or(bf2 BoolFunction) BoolFunction {
-	t := BoolFunction{
-		value:     make([]block, bf.rows),
-		rows:      bf.rows,
-		n:         bf.n,
-		m:         bf.m,
-		wasteBits: blockSize() - bf.m,
+//and() - покомпонентное сложение двух таблиц значений ВБФ
+func (bf VectorBoolFunction) or(bf2 VectorBoolFunction) VectorBoolFunction {
+	t := VectorBoolFunction{
+		value:      make([]block, bf.rows),
+		rows:       bf.rows,
+		n:          bf.n,
+		m:          bf.m,
+		wastedBits: blockSize() - bf.m,
 	}
 
 	for i := 0; i < bf.rows; i++ {
@@ -189,17 +210,19 @@ func (bf BoolFunction) or(bf2 BoolFunction) BoolFunction {
 	return t
 }
 
-func (bf BoolFunction) generateMask(step int) BoolFunction {
-	mask := BoolFunction{
-		value:     make([]block, bf.rows),
-		rows:      bf.rows,
-		n:         bf.n,
-		m:         bf.m,
-		wasteBits: blockSize() - bf.m,
+//generateMask() - генерирует функцию в которой каждая координата представляет собой
+//чередование step нулей и единиц
+func (bf VectorBoolFunction) generateMask(step int) VectorBoolFunction {
+	mask := VectorBoolFunction{
+		value:      make([]block, bf.rows),
+		rows:       bf.rows,
+		n:          bf.n,
+		m:          bf.m,
+		wastedBits: blockSize() - bf.m,
 	}
 	m := block(1<<blockSize() - 1)
 	for i := range mask.value {
-		if (i % (1 << step)) == 0 {
+		if (i % (step)) == 0 {
 			m ^= block(1<<blockSize() - 1)
 		}
 		mask.value[i] = m
@@ -207,36 +230,47 @@ func (bf BoolFunction) generateMask(step int) BoolFunction {
 	return mask
 }
 
-func (bf BoolFunction) Moebius() BoolFunction {
-	anf := BoolFunction{
-		value:     make([]block, bf.rows),
-		rows:      bf.rows,
-		n:         bf.n,
-		m:         bf.m,
-		wasteBits: blockSize() - bf.m,
+// Moebius - метод, выполняющий преобразование мёбиуса над заданной функцией.
+// возвращает полученную после преобразования функцию
+func (bf VectorBoolFunction) Moebius() VectorBoolFunction {
+	anf := VectorBoolFunction{
+		value:      make([]block, bf.rows),
+		rows:       bf.rows,
+		n:          bf.n,
+		m:          bf.m,
+		wastedBits: blockSize() - bf.m,
 	}
 
 	for i := range bf.value {
 		anf.value[i] = bf.value[i]
 	}
 	for i := 0; i < anf.n; i++ {
-		anf = anf.xor(anf.shiftDown(1 << i).and(anf.generateMask(i)))
+		anf = anf.xor(anf.shiftDown(1 << i).and(anf.generateMask(1 << i)))
 	}
 	return anf
 }
 
-func (bf BoolFunction) printANF() string {
+// printANF() - возвращает строку, в которой перечислены АНФ для каждой координаты ВБФ
+func (bf VectorBoolFunction) printANF() string {
+	//Если переменных больше 26, то в английском языке не хватит букв.
+	//Хотя смысла от такого представления нет уже при n = 26
 	if bf.n > 26 {
 		return "too many variables in ANF"
 	}
 
+	//создаём массив строк, по строке для каждой координаты
 	ANFs := make([]string, bf.m)
+
+	//Для каждой координаты првоеряем. нужна ли единица в сумме
 	for j := 0; j < bf.m; j++ {
 		if (bf.value[0]>>(blockSize()-j-1))&1 == 1 {
 			ANFs[j] += "1"
 		}
 	}
+
+	//Идём по значениями функций
 	for i := 1; i < bf.rows; i++ {
+		//Для каждой координаты првоеряем, нужно ли слагаемое на данном значении переменных
 		for j := 0; j < bf.m; j++ {
 			if (bf.value[i]>>(blockSize()-j-1))&1 == 1 {
 				if len(ANFs[j]) != 0 {
@@ -255,7 +289,7 @@ func (bf BoolFunction) printANF() string {
 	for i := range ANFs {
 		res += "anf" + strconv.Itoa(i) + " = "
 		if len(ANFs[i]) == 0 {
-			res += "0"
+			res += "0" //Если координата состоит из нулей, то её АНФ = 0
 		} else {
 			res += ANFs[i]
 		}
