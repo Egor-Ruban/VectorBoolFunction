35679b2d11b7a1ed770a0de6f5d2b3e639eb63ed
user3 <qwerty123>
2022-03-29 23:55:25 +0700

    вынес тест в отдельную функцию

--- main.go
+++ main.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"fmt"
-	"github.com/xuri/excelize/v2"
 	_ "github.com/xuri/excelize/v2"
-	"strconv"
 )
 
 type Test struct {
@@ -13,38 +10,7 @@ type Test struct {
 }
 
 func main() {
-	name := "NonDegenerateExperiments"
-	f := excelize.NewFile()
-	// Set value of a cell.
-	f.SetCellValue("Sheet1", "A1", "n")
-	f.SetCellValue("Sheet1", "B1", "m")
-	f.SetCellValue("Sheet1", "C1", "sum")
-	f.SetCellValue("Sheet1", "D1", "count")
-	f.SetCellValue("Sheet1", "E1", "p")
-	res := make([][]Test, 40)
-	for i := range res {
-		res[i] = make([]Test, 40)
-	}
-	for i := 0; i < 5000; i++ {
-		for n := 1; n < 10; n++ {
-			for m := 1; m <= 30; m++ {
-				bf, _ := newRevVBF(n, m)
-				if bf.isNonDegenerate() {
-					res[n][m].sum++
-				}
-				res[n][m].count++
-				i := strconv.Itoa(1 + ((n - 1) * 30) + m)
-				f.SetCellValue("Sheet1", "A"+i, n)
-				f.SetCellValue("Sheet1", "B"+i, m)
-				f.SetCellValue("Sheet1", "C"+i, res[n][m].sum)
-				f.SetCellValue("Sheet1", "D"+i, res[n][m].count)
-				f.SetCellValue("Sheet1", "E"+i, float64(res[n][m].sum)/float64(res[n][m].count))
-			}
-		}
-	}
-	if err := f.SaveAs(name + ".xlsx"); err != nil {
-		fmt.Println(err)
-	}
+	nonDegenerateTest()
 }
 
 func pow2(n int) int {
---
+++ tests.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"github.com/xuri/excelize/v2"
+	"strconv"
+)
+
+func nonDegenerateTest() {
+	name := "NonDegenerateExperiments"
+	f := excelize.NewFile()
+	// Set value of a cell.
+	f.SetCellValue("Sheet1", "A1", "n")
+	f.SetCellValue("Sheet1", "B1", "m")
+	f.SetCellValue("Sheet1", "C1", "sum")
+	f.SetCellValue("Sheet1", "D1", "count")
+	f.SetCellValue("Sheet1", "E1", "p")
+	res := make([][]Test, 40)
+	for i := range res {
+		res[i] = make([]Test, 40)
+	}
+	for i := 0; i < 5000; i++ {
+		for n := 1; n < 7; n++ {
+			for m := n; m <= 32; m++ {
+				bf, _ := newRevVBF(n, m)
+				if bf.isNonDegenerate() {
+					res[n][m].sum++
+				}
+				res[n][m].count++
+				i := strconv.Itoa(1 + ((n - 1) * 30) + m)
+				f.SetCellValue("Sheet1", "A"+i, n)
+				f.SetCellValue("Sheet1", "B"+i, m)
+				f.SetCellValue("Sheet1", "C"+i, res[n][m].sum)
+				f.SetCellValue("Sheet1", "D"+i, res[n][m].count)
+				f.SetCellValue("Sheet1", "E"+i, float64(res[n][m].sum)/float64(res[n][m].count))
+			}
+		}
+	}
+	if err := f.SaveAs(name + ".xlsx"); err != nil {
+		fmt.Println(err)
+	}
+}
